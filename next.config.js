2642b358bd3e7cd2c297f9ca9dbced3d05362938
NPC64927130 <NPC64927130@memetic.institute>
2022-03-20 17:26:39 -0400

    chore: use next.js swc compiler

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true',
 });
 
 const plugins = [withBundleAnalyzer];
 
 const nextConfig = {
-    webpack(config /* options */) {
-        return config;
-    },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 };
 
 module.exports = withPlugins(plugins, nextConfig);
